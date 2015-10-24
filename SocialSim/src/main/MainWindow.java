40f076e664109afef0c121c1da3ea275070c646b
JFleming4 <justinfleming95@gmail.com>
2015-10-24 10:46:05 -0400

    The basic layout is set up with exception of spacing and graph

--- SocialSim/src/main/MainWindow.java
+++ SocialSim/src/main/MainWindow.java
@@ -1,6 +1,6 @@
 package main;
 
-import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
@@ -14,7 +14,7 @@ import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 /**
- * 
+ *
  * @author Justin Fleming
  * Create and maintain the GUI for the user
  */
@@ -24,35 +24,35 @@ public class MainWindow extends JFrame {
 	private JTextField numberOfConsField; // text field to get # of consumers in sim
 	private JTextField numberOfTagsField; // text field to get # of tags in sim
 	private JTextField numberOfSearchResultsField; // text field to get # of search results returned each turn
-	
+
 	private JButton startButton;
-	
+
 	private DefaultTableModel documentsTableModel;
 	private DefaultTableModel consumersTableModel;
 	private DefaultTableModel producersTableModel;
-	
+
 	private int numberOfTurns;	// number of turns in the sim
 	private int numberOfProds;  // number of Producers in sim
 	private int numberOfCons;   // number of Consumers in sim
 	private int numberOfTags;   // number of Tags in sim
 	private int numberOfSearchResults; // number of Search Results each turn
-	
+
 	public MainWindow(){
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		GridBagLayout layout = new GridBagLayout();
 		GridBagConstraints con = new GridBagConstraints();
-		
+		JPanel panel = new JPanel(layout);
 		// Initialize Components
-		numberOfTurnsField 			= new JTextField();
-		numberOfProdsField 			= new JTextField();
-		numberOfConsField 			= new JTextField();
-		numberOfTagsField 			= new JTextField();
-		numberOfSearchResultsField 	= new JTextField();
+		numberOfTurnsField 			= new JTextField(80);
+		numberOfProdsField 			= new JTextField(80);
+		numberOfConsField 			= new JTextField(80);
+		numberOfTagsField 			= new JTextField(80);
+		numberOfSearchResultsField 	= new JTextField(80);
 		startButton					= new JButton("Start");
 		documentsTableModel			= new DefaultTableModel(5, 4);
 		consumersTableModel			= new DefaultTableModel();
 		producersTableModel			= new DefaultTableModel();
-		
+
 		// Other Components		-- Ones we arn't keeping track of
 		JLabel documentTableLabel 	= new JLabel("Documents");
 		JLabel consumerTableLabel	= new JLabel("Consumers");
@@ -60,46 +60,50 @@ public class MainWindow extends JFrame {
 		String documentTableHeader[] = {"ID", "Tag(s)", "Likes", "ProducerID"};
 		String consumerTableHeader[] = {"ID", "Tag(s)", "Followers", "Following", "Payoff"};
 		String producerTableHeader[] = {"ID", "Tag(s)", "Followers", "Following", "Payoff", "Documents"};
-		
+
 		// Build Top Display Bar -- First Row
 		con.weightx = 0.0;
-		layout.addLayoutComponent(buildLabelField("Turns:", numberOfTurnsField), con);
-		layout.addLayoutComponent(buildLabelField("Tags:", numberOfTagsField), con);
-		layout.addLayoutComponent(buildLabelField("Producers:", numberOfProdsField), con);
-		layout.addLayoutComponent(buildLabelField("Consumers:", numberOfConsField), con);
-		layout.addLayoutComponent(buildLabelField("Search Results:", numberOfSearchResultsField), con);
-		
+		con.gridwidth = 1;
+		panel.add(buildLabelField("Turns:", numberOfTurnsField), con);
+		panel.add(buildLabelField("Tags:", numberOfTagsField), con);
+		panel.add(buildLabelField("Producers:", numberOfProdsField), con);
+		panel.add(buildLabelField("Consumers:", numberOfConsField), con);
+		panel.add(buildLabelField("Search Results:", numberOfSearchResultsField), con);
+
 		con.gridwidth = GridBagConstraints.REMAINDER;	// End of Row
-		layout.addLayoutComponent(startButton, con);
-		
+		panel.add(startButton, con);
+
 		// Build Table Headers -- Second Row
 		con.weightx = 0.33;
-		layout.addLayoutComponent(documentTableLabel, con);
-		layout.addLayoutComponent(consumerTableLabel, con);
-		
+		con.gridwidth = 2;
+		panel.add(documentTableLabel, con);
+		panel.add(consumerTableLabel, con);
+
 		con.gridwidth = GridBagConstraints.REMAINDER;	// End of row
-		layout.addLayoutComponent(producerTableLabel, con);
-		
+		panel.add(producerTableLabel, con);
+
 		//Build Tables 	-- Third to eighth rows
 		con.gridheight = 5;
-		layout.addLayoutComponent(buildTable(documentsTableModel, documentTableHeader), con);
-		layout.addLayoutComponent(buildTable(consumersTableModel, consumerTableHeader), con);
-		
+		con.gridwidth = 2;
+		panel.add(buildTable(documentsTableModel, documentTableHeader), con);
+		panel.add(buildTable(consumersTableModel, consumerTableHeader), con);
+
 		con.gridwidth = GridBagConstraints.REMAINDER;  // End of row
-		layout.addLayoutComponent(buildTable(producersTableModel, producerTableHeader), con);
-		
+		panel.add(buildTable(producersTableModel, producerTableHeader), con);
+
 		//Build Graph   -- Ninth to  rows
 		//con.gridheight = 4;
-		JPanel panel = new JPanel(layout);
+		
 		getContentPane().add(panel);
+		setSize(600,400);
 		setVisible(true);
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.println("In main");
 		MainWindow win = new MainWindow();
 	}
-	
+
 	/**
 	 * Create a Text Field with a JLabel beside it
 	 * @param label, text for the JLabel
@@ -109,20 +113,20 @@ public class MainWindow extends JFrame {
 	private JPanel buildLabelField(String label, JTextField field) {
 		GridBagLayout layout = new GridBagLayout();
 		GridBagConstraints con = new GridBagConstraints();
-		
+		field.setMinimumSize(new Dimension(35, 25));
 		con.fill = GridBagConstraints.BOTH;
 		con.weightx = 0.0;
-		
+
 		JPanel labelField = new JPanel(layout);
 		labelField.add(new JLabel(label), con);
-		
+
 		con.weighty = 1.0;
 		labelField.add(field, con);
-		
+
 		return labelField;
 	}
-	
-	
+
+
 	/**
 	 * Creates a ScrollPane containing a JTable with tableModel and header
 	 * @param tableModel, DefaultTableModel to be used by the JTable
@@ -132,6 +136,8 @@ public class MainWindow extends JFrame {
 	private JScrollPane buildTable(DefaultTableModel tableModel, String[] header) {
 		tableModel.setColumnIdentifiers(header);
 		JTable table = new JTable(tableModel);
-		return new JScrollPane(table);
+		JScrollPane pane = new JScrollPane(table);
+		pane.setMinimumSize(new Dimension(300,200));
+		return pane;
 	}
 }
