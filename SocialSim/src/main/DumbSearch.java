63b6295811f2c32c22dac61e5627e029a738fa2d
Reid Cain-Mondoux <reidcainmondoux@gmail.com>
2015-10-29 12:12:15 -0400

    Added DumbSearch

---
+++ SocialSim/src/main/DumbSearch.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DumbSearch implements Search {
+
+	@Override
+	public List<Document> search(Consumer consumers, List<Document> documents, int k) {
+		// TODO Auto-generated method stub
+		if(k>documents.size())
+			return documents;
+		
+		List<Document> documentsCopy = new ArrayList<Document>(documents);
+		//sorting based on popularity
+		Collections.sort(documentsCopy);
+	
+		return documentsCopy;
+	}
+
+}
